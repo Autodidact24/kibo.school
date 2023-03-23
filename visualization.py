9e1d357ef2af0b83cdc91f11743db271acb12cef
Shubham Tomar <tomarshubham24@gmail.com>
2023-03-23 15:42:08 +0530

    starter code for visualizations

---
+++ visualization.py
@@ -0,0 +1,14 @@
+# libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# TODO: Read the Dataset using pandas
+df = pd.read_csv("co2_trends.csv")
+
+# TODO: Plot the mean temperature for each month since 2013
+
+
+
+# show the graph
+plt.show()
